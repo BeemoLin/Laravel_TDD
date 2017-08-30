d1e0ce42c93732ef41900648e10c64ef7b216d46
vagrant <vagrant@homestead>
2017-08-30 06:45:28 +0000

    A Thread Can Have Replies

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -8,26 +8,36 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 use App\Models\Thread;
+use App\Models\Reply;
 
 class ThreadsTest extends TestCase
 {
+    //use DatabaseMigrations;
+
+    public function setUp()
+    {
+        parent::setUp();
+ 
+        $this->thread = factory(Thread::class)->create();
+    }
+
     /** @test **/
     public function a_user_can_view_all_threads()
     {
-        $thread = factory(Thread::class)->create();
-
-        $response = $this->get('/threads');
-
-        $response->assertSee($thread->title);
+        $response = $this->get('/threads')->assertSee($this->thread->title);
     }
 
     /** @test **/
     public function a_user_can_read_a_single_thread()
     {
-        $thread = factory(Thread::class)->create();
+        $response = $this->get('/threads/' . $this->thread->id)->assertSee($this->thread->title);
+    }
 
-        $response = $this->get('/threads/' . $thread->id);
+    /** @test **/
+    public function a_user_can_read_replies_that_are_associated_with_a_thread()
+    {
+        $reply = factory(Reply::class)->create(['thread_id' => $this->thread->id]);
 
-        $response->assertSee($thread->title);
+        $this->get('/threads/' . $this->thread->id)->assertSee($reply->body);
     }
 }
